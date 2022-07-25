93c667e6c161e2f48569b4dd49dbd27278432706
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-25 17:10:34 +0300

    Default properties

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -10,6 +10,11 @@ const GroupList = ({ items, valueProperty, contentProperty }) => {
     );
 };
 
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+
 GroupList.propTypes = {
     items: PropTypes.object.isRequired,
     valueProperty: PropTypes.string.isRequired,
