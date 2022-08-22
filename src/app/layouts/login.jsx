3a75088be8b851e5ff0d4278d6655a0432a461ac
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-22 15:36:01 +0300

    9. Forms. Part I. Reusable enter field

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
     const [data, setData] = useState({ email: "", password: "" });
 
     const handleChange = ({ target }) => {
-        // setEmail(e.target.value);
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
@@ -15,22 +13,8 @@ const Login = () => {
 
     return (
         <form action="">
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" value={data.email} onChange={handleChange} />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" value={data.password} onChange={handleChange} />
-            </div>
-            <div>
-                <label htmlFor="radio1">Radio 1</label>
-                <input type="radio" id="radio1" name="radio" />
-            </div>
-            <div>
-                <label htmlFor="radio2">Radio 2</label>
-                <input type="radio" id="radio2" name="radio" />
-            </div>
+            <TextField label="Email" name="email" value={data.email} onChange={handleChange} />
+            <TextField label="Password" type="password" name="password" value={data.password} onChange={handleChange} />
         </form>
     );
 };
