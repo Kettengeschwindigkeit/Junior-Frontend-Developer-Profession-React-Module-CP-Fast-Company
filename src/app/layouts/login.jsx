9c139e353701e601dad6b4aa290814017a21e1a8
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-22 14:49:58 +0300

    9. Forms. Part I. Controllable & Non controllable components

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+        console.log(e.target.value);
+    };
+
     return (
         <form action="">
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input type="text" id="email" value={email} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
