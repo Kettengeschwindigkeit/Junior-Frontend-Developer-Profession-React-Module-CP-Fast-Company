f8de79c3cf092ebbeb33633555d012fb4ced4a70
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-22 15:13:45 +0300

    9. Forms. Part I. Handling several fields values

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    const [data, setData] = useState({ email: "", password: "" });
 
-    const handleChange = (e) => {
-        setEmail(e.target.value);
-        console.log(e.target.value);
+    const handleChange = ({ target }) => {
+        // setEmail(e.target.value);
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
     };
 
     return (
         <form action="">
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" value={email} onChange={handleChange} />
+                <input type="text" id="email" name="email" value={data.email} onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" name="password" value={data.password} onChange={handleChange} />
+            </div>
+            <div>
+                <label htmlFor="radio1">Radio 1</label>
+                <input type="radio" id="radio1" name="radio" />
+            </div>
+            <div>
+                <label htmlFor="radio2">Radio 2</label>
+                <input type="radio" id="radio2" name="radio" />
             </div>
         </form>
     );
