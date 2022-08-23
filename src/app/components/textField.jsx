f44e062c801bd2c1fafc3971caca3bf849872511
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-23 09:39:23 +0300

    9. Forms. Part I. Adding Bootstrap styles

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import PropTypes, { string } from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const getInputClasses = () => {
+        return "form-control" + (error ? " is-invalid" : "");
+    };
+
     return (
-        <div>
+        <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input type={type} id={name} name={name} value={value} onChange={onChange} />
-            {error && <p>{error}</p>}
+            <input className={getInputClasses()} type={type} id={name} name={name} value={value} onChange={onChange} />
+            {error && <div className="invalid-feedback">{error}</div>}
         </div>
     );
 };
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -59,11 +59,18 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField label="Email" name="email" value={data.email} onChange={handleChange} error={errors.email} />
-            <TextField label="Password" type="password" name="password" value={data.password} onChange={handleChange} error={errors.password} />
-            <button type="submit" disabled={!isValid}>Submit</button>
-        </form>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 p-4 shadow">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField label="Email" name="email" value={data.email} onChange={handleChange} error={errors.email} />
+                        <TextField label="Password" type="password" name="password" value={data.password} onChange={handleChange} error={errors.password} />
+                        <button className="btn btn-primary w-100 mx-auto" type="submit" disabled={!isValid}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
