c806d0216f338071a2e024ac329aefd271835edc
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-23 10:07:23 +0300

    9. Forms. Part I. Show / hide password

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes, { string } from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
     const getInputClasses = () => {
         return "form-control" + (error ? " is-invalid" : "");
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
     };
 
     return (
         <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input className={getInputClasses()} type={type} id={name} name={name} value={value} onChange={onChange} />
+            <div className="input-group has-validation">
+                <input className={getInputClasses()} type={showPassword ? "text" : type} id={name} name={name} value={value} onChange={onChange} />
+                {type === "password" && <button className="btn btn-outline-secondary" type="button" onClick={toggleShowPassword}>
+                    <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+                </button>}
+            </div>
             {error && <div className="invalid-feedback">{error}</div>}
         </div>
     );
