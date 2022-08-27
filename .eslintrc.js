2bb7c0cbf4e500bd155b7b950287e89f0d3b0592
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-27 18:23:17 +0300

    10. Forms. Part II. Rgister form

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
     rules: {
         indent: ["error", 4],
         semi: [2, "always"],
-        "space-before-function-paren": ["error", { anonymous: "always", named: "never"}],
+        "space-before-function-paren": ["error", { anonymous: "always", named: "never" }],
+        "multiline-ternary": ["off"],
         quotes: ["error", "double", { allowTemplateLiterals: true }]
     }
 };
