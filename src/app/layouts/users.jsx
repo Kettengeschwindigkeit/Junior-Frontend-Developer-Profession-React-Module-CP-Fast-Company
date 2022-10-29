99e1c00670d3b1e27c244d055e834500d41916f2
Kettengeschwindigkeit <666damned999@mail.ru>
2022-10-29 13:10:39 +0300

    14. API, custom hooks, state management. Adding HTTP-Service

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,10 +3,17 @@ import { useParams } from "react-router-dom";
 import UserEditPage from "../components/page/editUserPage/userEditPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
+import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
-    return <>{userId ? edit ? <UserEditPage userId={userId} /> : <UserPage userId={userId} /> : <UsersListPage />}</>;
+    return (
+        <>
+            <UserProvider>
+                {userId ? edit ? <UserEditPage userId={userId} /> : <UserPage userId={userId} /> : <UsersListPage />}
+            </UserProvider>
+        </>
+    );
 };
 
 export default Users;
