4f8ab0f669ed2feb55af05dbb014943d83362547
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-14 16:05:16 +0300

    20. Fast Company Redux. Changing ProtectedRoutes

--- src/app/components/protectedRoute.jsx
+++ src/app/components/protectedRoute.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
 import PropTypes from "prop-types";
+import { getIsLoggedIn } from "../store/users";
 import { useAuth } from "../hooks/useAuth";
 
 const ProtectedRoute = ({ component: Component, children, ...rest }) => {
+    const isLoggedIn = useSelector(getIsLoggedIn());
+
     const { currentUser } = useAuth();
 
     return (
         <Route {...rest} render={(props) => {
-            if (!currentUser) {
+            if (!isLoggedIn) {
                 return <Redirect to={{ pathname: "/login", state: { from: props.location } }} />;
             } else if (rest.computedMatch.params.edit && (currentUser._id !== rest.computedMatch.params.userId)) {
                 return <Redirect to={{ pathname: `/users/${currentUser._id}`, state: { from: props.location } }} />;
--- src/app/store/users.js
+++ src/app/store/users.js
@@ -27,7 +27,8 @@ const usersSlice = createSlice({
             state.isLoading = false;
         },
         authRequestSuccess: (state, action) => {
-            state.auth = { ...action.payload, isLoggedIn: true };
+            state.auth = action.payload;
+            state.isLoggedIn = true;
         },
         authRequestFailed: (state, action) => {
             state.error = action.payload;
@@ -105,5 +106,7 @@ export const getUserById = (userId) => state => {
         return state.users.entities.find(u => u._id === userId);
     }
 };
+
+export const getIsLoggedIn = () => (state) => state.users.isLoggedIn;
 
 export default usersReducer;
