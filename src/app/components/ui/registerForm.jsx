b8d2a2d93b4b23d1438453d4b0979d38448793b3
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-14 09:04:21 +0300

    20. Fast Company Redux. Moving register method in UserSlice

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -5,13 +5,12 @@ import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radioField";
 import MultiSelectField from "../common/form/multiSelectField";
 import CheckBoxField from "../common/form/checkBoxField";
-import { useAuth } from "../../hooks/useAuth";
-import { useHistory } from "react-router-dom";
 import { getQualities } from "../../store/qualities";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { signUp } from "../../store/users";
 
 const RegisterForm = () => {
-    const history = useHistory();
+    const dispatch = useDispatch();
     const [data, setData] = useState({
         email: "",
         password: "",
@@ -21,8 +20,6 @@ const RegisterForm = () => {
         qualities: [],
         licence: false
     });
-
-    const { signUp } = useAuth();
 
     const professions = useSelector(state => state.professions.entities);
     const qualities = useSelector(getQualities());
@@ -95,7 +92,7 @@ const RegisterForm = () => {
     };
     const isValid = Object.keys(errors).length === 0;
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const isValid = validate();
         if (!isValid) return;
@@ -103,12 +100,7 @@ const RegisterForm = () => {
             ...data,
             qualities: data.qualities.map((q) => q.value)
         };
-        try {
-            await signUp(newData);
-            history.push("/");
-        } catch (error) {
-            setErrors(error);
-        }
+        dispatch(signUp(newData));
     };
 
     return (
